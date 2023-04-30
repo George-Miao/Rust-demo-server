a53514b74463fd0372741c201381646d5ac4903d
George Miao <gm@miao.dev>
2023-04-30 17:25:24 -0400

    feat: adapt to zeabur

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+use std::net::SocketAddrV4;
+
 use axum::{http::StatusCode, routing::get, Router};
 
 #[tokio::main]
 async fn main() {
+    let port = std::env::var("PORT")
+        .expect("PORT is not set")
+        .parse()
+        .expect("PORT is not valid");
     let app = Router::new()
         .route("/", get(|| async { "Hello, world!" }))
         .route("/health", get(|| async { StatusCode::NO_CONTENT }));
 
-    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+    axum::Server::bind(&SocketAddrV4::new("0.0.0.0".parse().unwrap(), port).into())
         .serve(app.into_make_service())
         .await
         .unwrap();
